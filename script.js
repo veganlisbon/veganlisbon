d1d42a62e6968c1bb4c137d50ec881eecd2b6e62
Filipa Marta <filipa.marta@gmail.com>
2020-03-25 17:44:25 +0000

    menu bar

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const select = document.getElementById("kindOfBusiness")
 const business = document.getElementById("nameBusiness")
 
 select.addEventListener('change', function(event){business.innerHTML = `Name of your ${event.target.value}`})
+
+
